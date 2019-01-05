e2dba22393efb131fa6340704970cd68d937591c
niujiaying <912768292@qq.com>
2019-01-05 08:58:03 +0800

    第一遍首页的公共部分功能完成

--- public/back-my01/js/common.js
+++ public/back-my01/js/common.js
@@ -15,3 +15,52 @@ $(document).ajaxStop(function(){
     NProgress.done();
     },200)
 })
+
+
+
+
+$(function(){
+    // 公共的功能:
+   // 需求1:点击categoryBtn按钮,二级菜单显示
+
+   $('.lt_aside .categoryBtn').on('click',function(){
+       
+       $(this).next().stop().slideToggle();
+   })
+
+//    需求2:点击左侧的菜单按钮,左侧侧边栏向左侧移动,topbar向左侧移动,
+   $(".icon_menu").on("click",function(){
+       $(".lt_aside").toggleClass("remove");
+       $(".lt_topBar").toggleClass("remove");
+       $(".lt_main").toggleClass("remove");
+        
+   })
+//    需求3:点击退出按钮,展示模态框
+// $('#myModal').modal(options)
+
+$(".icon_logout").on("click",function(){
+    $("#myModal").modal("show");
+     
+
+})
+
+
+$("#logoutBtn").on("click",function(){
+ 
+        
+    $.ajax({
+        url:"/employee/employeeLogout",
+        type:"get",
+        dataType:"json",
+        success:function(info){
+            
+            if(info.success){
+                location.href="login.html";
+            }
+             
+        }
+    })
+})
+   
+     
+})
